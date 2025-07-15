290a05ad776581d81da3b21c1c63e0e16c013313
chunho <chunhoes.inbox@gmail.com>
2025-07-16 07:40:05 +0800

    Added __init__.py package

---
+++ src/website/__init__.py
@@ -0,0 +1,27 @@
+# Purpose of this file: 
+# Initialises Flask app + SQLAlchemy
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'qwertyuiop'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    from .routes import directories
+
+    app.register_blueprint(directories, url_prefix='/')
+    
+    return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database')
